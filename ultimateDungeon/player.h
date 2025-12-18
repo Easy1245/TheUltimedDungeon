ec4b08371c165fccb42fefee72ca6cd617bbfa78
JonasVanmarsenille <120404980+Easy1245@users.noreply.github.com>
2025-12-18 17:49:38 +0100

    Optimize player attributes to use unsigned char
    Changed health, damage, and defense variables from int to unsigned char for memory optimization.

--- ultimateDungeon/player.h
+++ ultimateDungeon/player.h
@@ -6,21 +6,26 @@
 #include <algorithm>
 #include <iostream>
 #include "Room.h"
+#include "Utils.h"
 
 namespace dungeon {
 
 class Game;
 
 class Player {
+    friend class Game;
+
 private:
     std::string name;
     Room* currentRoom;
-    int health;
-    int damage;
-    int defense;
+
+    unsigned char health;
+    unsigned char damage;
+    unsigned char defense;
+
+    bool alive;
+
     std::vector<std::string> inventory;
-    unsigned char level;
-    unsigned char lives;
 
 public:
     Player();
@@ -29,22 +34,19 @@ public:
     ~Player();
 
     const std::string& getName() const;
+
     Room* getRoom() const;
-    int getHealth() const;
-    int getDamage() const;
-    int getDefense() const;
-
-    void setHealth(int h);
-    void setDamage(int d);
-    void setDefense(int d);
-
     void moveTo(Room* r);
-    void addItem(const std::string& item);
-    void takeDamage(int dmg = 1);
 
-    friend class Game;
+    unsigned char getHealth() const;
+    unsigned char getDamage() const;
+    unsigned char getDefense() const;
+
+    void takeDamage(unsigned char dmg = 1);
+
+    void addItem(const std::string& item);
 };
 
-} // namespace dungeon
+}
 
 #endif
