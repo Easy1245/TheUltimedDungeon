ee21c0126d68cf7e8d609626c67c9c54d783180a
JonasVanmarsenille <120404980+Easy1245@users.noreply.github.com>
2025-12-19 02:54:05 +0100

    Implement Monster class in Monster.cpp

---
+++ ultimateDungeon/Monster.cpp
@@ -0,0 +1,21 @@
+#include "Monster.h"
+#include <cstdlib>
+
+namespace dungeon {
+
+Monster::Monster()
+    : Enemy("Default Monster", 20) {}
+
+Monster::Monster(const std::string& n, int h)
+    : Enemy(n, h) {}
+
+Monster::Monster(const Monster& other)
+    : Enemy(other) {}
+
+Monster::~Monster() {}
+
+int Monster::attack() const {
+    return (std::rand() % 16) + 5;
+}
+
+} // namespace dungeon
