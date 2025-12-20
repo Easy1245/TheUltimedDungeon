2f36f26fe58b5884f234c38ce06ea774e9a9b513
JonasVanmarsenille <120404980+Easy1245@users.noreply.github.com>
2025-12-20 20:56:44 +0100

    Update Dungeonbuilder.cpp

--- ultimateDungeon/Dungeonbuilder.cpp
+++ ultimateDungeon/Dungeonbuilder.cpp
@@ -1,19 +1,30 @@
 #include "Dungeonbuilder.h"
 #include "Monster.h"
+#include "Room.h"
+#include "Player.h"
 
 namespace dungeon {
 
-Player DungeonBuilder::build(const std::string& playerName, std::vector<std::unique_ptr<Room>>& rooms)
+Player DungeonBuilder::build(
+    const std::string& playerName,
+    std::vector<std::unique_ptr<Room>>& rooms
+    )
 {
     rooms.push_back(std::make_unique<Room>(0, "Lumbrig", "Start of a new journey"));
-    rooms.push_back(std::make_unique<Room>(1, "Dark forest", ""));
+    rooms.push_back(std::make_unique<Room>(1, "Dark Forest", "Forest filled with darkness"));
+    rooms.push_back(std::make_unique<Room>(2, "Dragon Den", "Home of Smaug the dragon"));
+    rooms.push_back(std::make_unique<Room>(3, "Olympus", "Home of the Greek gods"));
+    rooms.push_back(std::make_unique<Room>(4, "Tilted Towers", "Where we droppen boys"));
 
     rooms[0]->connect(rooms[1].get());
+    rooms[1]->connect(rooms[2].get());
+    rooms[0]->connect(rooms[4].get());
+    rooms[2]->connect(rooms[3].get());
 
-    rooms[1]->setEnemy(std::unique_ptr<Enemy>(new Monster("Goblin", 30)));
+    rooms[1]->setEnemy(std::make_unique<Monster>("Goblin", 30));
+    rooms[3]->setEnemy(std::make_unique<Monster>("Zeus", 200));
 
-
-    return Player("Hero", rooms[0].get());
+    return Player(playerName, rooms[0].get());
 }
 
 } // namespace dungeon
